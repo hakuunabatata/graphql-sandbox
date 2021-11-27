1661d8dd2ebc1c25cda1222556db7bd63564e944
hakuunabatata <emachadomartins1999@gmail.com>
2021-11-27 12:30:16 -0300

    join users with roles

--- api/user/datasource/user.js
+++ api/user/datasource/user.js
@@ -7,9 +7,22 @@ class UsersAPI extends RESTDataSource {
         this.baseURL = 'http://localhost:3000'
     }
 
-    getUsers = async () => this.get('/users')
+    getUsers = async () => {
+        const users = await this.get('/users')
 
-    getUserById = async (id) => this.get(`/users/${id}`)
+        return users.map(async ({ id, name, email, active, role }) => ({
+            id, name, email, active, role: await this.get(`/roles/${role}`)
+        }))
+    }
+
+    getUserById = async (id) => {
+
+        const { role, active, name, email } = await this.get(`/users/${id}`)
+
+        return ({ id, active, name, email, role: await this.get(`/roles/${role}`) })
+    }
+
+
 }
 
 module.exports = UsersAPI
