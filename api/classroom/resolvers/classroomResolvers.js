49c02b926306726827ed0920543dd2750c4a6b02
hakuunabatata <emachadomartins1999@gmail.com>
2021-12-01 08:14:02 -0300

    create classrooms schema

---
+++ api/classroom/resolvers/classroomResolvers.js
@@ -0,0 +1,19 @@
+const classrooms = [
+    {
+        id: 1,
+        description: 'basic'
+    },
+    {
+        id: 2,
+        description: 'medium'
+    }
+]
+
+
+const classRoomResolvers = {
+    Query: {
+        classrooms: (parent, args, context, info) => classrooms,
+        classroom: (parent, { id }, context, info) => classrooms.filter(({ id: classID }) => classID === id),
+
+    }
+}
--- api/index.js
+++ api/index.js
@@ -1,17 +1,23 @@
 const { ApolloServer } = require('apollo-server')
+// const { mergeTypeDefs } = require('graphql-tools')
+
 const userSchema = require('./user/schema/user.graphql')
 const userResolvers = require('./user/resolvers/userResolvers')
 const UsersAPI = require('./user/datasource/user')
+const classroomSchema = require('./classroom/schema/classroom.graphql')
+const classroomResolvers = require('./classroom/resolvers/classroomResolvers')
+// const ClassRoomsAPI = require('./classroom/datasource/classroom')
 
 
 
-const typeDefs = [userSchema]
+const typeDefs = [userSchema, classroomSchema]
 
-const resolvers = [userResolvers]
+const resolvers = [userResolvers, classroomResolvers]
 
 const server = new ApolloServer({
   typeDefs, resolvers, dataSources: () => ({
-    usersAPI: new UsersAPI()
+    usersAPI: new UsersAPI(),
+    // classRoomsAPI: new ClassRoomsAPI()
   })
 })
 
